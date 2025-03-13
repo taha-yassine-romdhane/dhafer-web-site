caa54b6b1cb41d8b494c15cce8c18d67f60aa3f0
taha yassine romdhane <taha.romdhane1999@gmail.com>
2025-03-13 19:56:48 +0100

    deployment fix errors 7

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
             <Navbar />
             <CartDropdown />
             <main className="min-h-screen">
+              {children}
             </main>
             <Footer />
           </CartProvider>
