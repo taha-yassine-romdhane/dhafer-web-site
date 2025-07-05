345cc554652bed0fd42a4af1ac59ed1ac5991d9b
taha yassine romdhane <taha.romdhane1999@gmail.com>
2025-07-05 01:51:44 +0100

    huge update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['images.aichic.tn'],
+  },
   output: 'standalone',
   experimental: {
     serverComponentsExternalPackages: [],
